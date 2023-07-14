b37ffc3b0ef07ad321e6b6dbd18950d723426b7e
Zhenxu Wang <mason123wp@gmail.com>
2023-07-13 22:31:51 -0400

    add annotations and service field

--- src/main/java/com/masonwang/pnp/web/TeamController.java
+++ src/main/java/com/masonwang/pnp/web/TeamController.java
@@ -1,4 +1,13 @@
 package com.masonwang.pnp.web;
 
+import com.masonwang.pnp.service.TeamService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/team")
 public class TeamController {
+    private TeamService teamService;
 }
--- src/main/java/com/masonwang/pnp/web/UserController.java
+++ src/main/java/com/masonwang/pnp/web/UserController.java
@@ -1,4 +1,13 @@
 package com.masonwang.pnp.web;
 
+import com.masonwang.pnp.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/user")
 public class UserController {
+    private UserService userService;
 }
