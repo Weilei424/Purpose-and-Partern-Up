2efa61bd841bc99f1c0c245ecbd3a1ea529cb308
Zhenxu Wang <mason123wp@gmail.com>
2023-07-14 21:55:08 -0400

    add methods

--- src/main/java/com/masonwang/pnp/service/ProposalService.java
+++ src/main/java/com/masonwang/pnp/service/ProposalService.java
@@ -1,7 +1,18 @@
 package com.masonwang.pnp.service;
 
+import com.masonwang.pnp.entity.Proposal;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Set;
 
 @Service
 public interface ProposalService {
+    Proposal getProposal(Long id);
+    Proposal saveProposal(Proposal proposal);
+    void deleteProposal(Long id);
+    Proposal updateProposal(Long id, Proposal proposal);
+    List<Proposal> getProposals();
+    Set<Proposal> getTeamProposals(Long teamId);
+    Set<Proposal> getUserProposals(Long userId);
 }
