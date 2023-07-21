820409985efd5dd3629025f8ca1552df7f417d84
Zhenxu Wang <mason123wp@gmail.com>
2023-07-20 21:48:37 -0400

    add CURD methods

--- src/main/java/com/masonwang/pnp/service/TeamService.java
+++ src/main/java/com/masonwang/pnp/service/TeamService.java
@@ -1,7 +1,14 @@
 package com.masonwang.pnp.service;
 
+import com.masonwang.pnp.entity.Team;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface TeamService {
+    Team getTeam(Long id);
+    Team saveTeam(Team team);
+    void deleteTeam(Long id);
+    Team updateTeam(Long id, Team team);
+    Team addMember(Long teamId, Long userId);
+
 }
