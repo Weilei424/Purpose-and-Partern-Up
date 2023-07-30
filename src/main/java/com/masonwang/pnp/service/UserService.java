5476416f5c020560fee405735e25780d4592c701
Zhenxu Wang <mason123wp@gmail.com>
2023-07-29 22:57:52 -0400

    add Crud methods

--- src/main/java/com/masonwang/pnp/service/UserService.java
+++ src/main/java/com/masonwang/pnp/service/UserService.java
@@ -1,7 +1,19 @@
 package com.masonwang.pnp.service;
 
+import com.masonwang.pnp.entity.Proposal;
+import com.masonwang.pnp.entity.Team;
+import com.masonwang.pnp.entity.User;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public interface UserService {
+    User getUser(Long id);
+    User saveUser(User user);
+    User updateUser(Long id, User user);
+    void deleteUser(Long id);
+    List<User> getUsers();
+    List<Team> getUserTeams(Long id);
+    List<Proposal> getUserProposals(Long id);
 }
--- src/main/java/com/masonwang/pnp/service/UserServiceImpl.java
+++ src/main/java/com/masonwang/pnp/service/UserServiceImpl.java
@@ -1,9 +1,57 @@
 package com.masonwang.pnp.service;
 
+import com.masonwang.pnp.entity.Proposal;
+import com.masonwang.pnp.entity.Team;
+import com.masonwang.pnp.entity.User;
+import com.masonwang.pnp.exception.EntityNotFoundException;
 import com.masonwang.pnp.repository.UserRepository;
 import lombok.AllArgsConstructor;
+
+import java.util.List;
+import java.util.Optional;
 
 @AllArgsConstructor
 public class UserServiceImpl implements UserService {
     private UserRepository userRepository;
+
+    @Override
+    public User getUser(Long id) {
+        Optional<User> user = userRepository.findById(id);
+        return unwrapUser(user, id);
+    }
+
+    @Override
+    public User saveUser(User user) {
+        return null;
+    }
+
+    @Override
+    public User updateUser(Long id, User user) {
+        return null;
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+
+    }
+
+    @Override
+    public List<User> getUsers() {
+        return null;
+    }
+
+    @Override
+    public List<Team> getUserTeams(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Proposal> getUserProposals(Long id) {
+        return null;
+    }
+
+    static User unwrapUser(Optional<User> entity, Long id) {
+        if (entity.isPresent()) return entity.get();
+        else throw new EntityNotFoundException(id, User.class);
+    }
 }
