26fb8c0cf4a0449abef375a2fb6701dac60737e8
Zhenxu Wang <mason123wp@gmail.com>
2023-07-29 20:38:15 -0400

    add attributes

--- src/main/java/com/masonwang/pnp/entity/Proposal.java
+++ src/main/java/com/masonwang/pnp/entity/Proposal.java
@@ -1,7 +1,21 @@
 package com.masonwang.pnp.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.NonNull;
+
+import java.util.List;
 
 @Entity
+@Table(name = "proposal")
 public class Proposal {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "id")
+    private Long id;
+
+    @NonNull
+    private String name;
+
+    private List<User> users;
 }
