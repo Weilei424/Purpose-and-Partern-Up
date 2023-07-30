4b05c8c37c0d6fcbd202df96dbb8dc0bdd12d090
Zhenxu Wang <mason123wp@gmail.com>
2023-07-29 22:58:22 -0400

    implement User.java

--- src/main/java/com/masonwang/pnp/entity/User.java
+++ src/main/java/com/masonwang/pnp/entity/User.java
@@ -1,7 +1,44 @@
 package com.masonwang.pnp.entity;
 
-import jakarta.persistence.Entity;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
 
+import java.util.List;
+import java.util.Set;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+@AllArgsConstructor
 @Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message =  "username cannot be blank")
+    @NonNull
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @NotBlank(message =  "password cannot be blank")
+    @NonNull
+    @Column(nullable = false)
+    private String password;
+
+    @JsonIgnore
+    List<Proposal> proposals;
+
+    @JsonIgnore
+    @ManyToMany
+    @JoinTable(
+            name = "user_team",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "team_id", referencedColumnName = "id")
+    )
+    Set<Team> teams;
 }
