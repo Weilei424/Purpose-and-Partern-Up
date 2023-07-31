de756017761c1680ecccf115c5b5e2fdfd4bdd2e
Zhenxu Wang <mason123wp@gmail.com>
2023-07-31 18:21:59 -0400

    add fields

--- src/main/java/com/masonwang/pnp/entity/Team.java
+++ src/main/java/com/masonwang/pnp/entity/Team.java
@@ -1,7 +1,32 @@
 package com.masonwang.pnp.entity;
 
-import jakarta.persistence.Entity;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
+import java.util.List;
+import java.util.Set;
 
 @Entity
 public class Team {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @JsonIgnore
+    @ManyToMany
+    @JoinTable(
+            name = "user_team",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "team_id", referencedColumnName = "id")
+    )
+    private Set<User> users;
+
+    @JsonIgnore
+    @ManyToOne
+    private List<Proposal> proposals;
+
 }
