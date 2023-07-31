74dbde4d05c287e47371a2f1ae028b69a3727faa
Zhenxu Wang <mason123wp@gmail.com>
2023-07-31 18:22:11 -0400

    add CRUD methods

--- src/main/java/com/masonwang/pnp/service/TeamServiceImpl.java
+++ src/main/java/com/masonwang/pnp/service/TeamServiceImpl.java
@@ -1,9 +1,35 @@
 package com.masonwang.pnp.service;
 
+import com.masonwang.pnp.entity.Team;
 import com.masonwang.pnp.repository.TeamRepository;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 public class TeamServiceImpl implements TeamService {
     private TeamRepository teamRepository;
+
+    @Override
+    public Team getTeam(Long id) {
+        return null;
+    }
+
+    @Override
+    public Team saveTeam(Team team) {
+        return null;
+    }
+
+    @Override
+    public void deleteTeam(Long id) {
+
+    }
+
+    @Override
+    public Team updateTeam(Long id, Team team) {
+        return null;
+    }
+
+    @Override
+    public Team addMember(Long teamId, Long userId) {
+        return null;
+    }
 }
