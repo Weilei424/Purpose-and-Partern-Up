89ff9c6e11eef550589496664cea4655ee8ab3ba
Zhenxu Wang <mason123wp@gmail.com>
2023-08-02 02:45:02 -0400

    wire up fields

--- src/main/java/com/masonwang/pnp/entity/Team.java
+++ src/main/java/com/masonwang/pnp/entity/Team.java
@@ -1,7 +1,36 @@
 package com.masonwang.pnp.entity;
 
-import jakarta.persistence.Entity;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
 
+import java.util.Set;
+
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Getter
+@Setter
 @Entity
+@Table(name = "team")
 public class Team {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @NotBlank(message = "Team name cannot be blank!")
+    @NonNull
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @JsonIgnore
+    @ManyToMany
+    @JoinTable(
+            name = "team_user",
+            joinColumns = @JoinColumn(name = "team_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id")
+    )
+    private Set<User> users;
 }
