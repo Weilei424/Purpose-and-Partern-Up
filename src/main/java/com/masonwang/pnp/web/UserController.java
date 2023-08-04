048534a2143616842010b4ae95bb7c356a9361a4
Zhenxu Wang <mason123wp@gmail.com>
2023-08-04 03:11:18 -0400

    implement GET and POST handlers for User

--- src/main/java/com/masonwang/pnp/web/UserController.java
+++ src/main/java/com/masonwang/pnp/web/UserController.java
@@ -1,13 +1,26 @@
 package com.masonwang.pnp.web;
 
+import com.masonwang.pnp.entity.User;
 import com.masonwang.pnp.service.UserService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @AllArgsConstructor
 @RestController
 @RequestMapping("/user")
 public class UserController {
     private UserService userService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@PathVariable Long id) {
+        return new ResponseEntity<>(userService.getUser(id), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> saveUser(@RequestBody User user) {
+        return new ResponseEntity<>(userService.saveUser(user), HttpStatus.CREATED);
+    }
 }
