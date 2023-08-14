4fb43d94b6f4f6900512d2e905814d438d02328b
Zhenxu Wang <mason123wp@gmail.com>
2023-08-14 03:22:32 -0400

    implement methods

--- src/main/java/com/masonwang/pnp/web/UserController.java
+++ src/main/java/com/masonwang/pnp/web/UserController.java
@@ -1,5 +1,7 @@
 package com.masonwang.pnp.web;
 
+import com.masonwang.pnp.entity.Proposal;
+import com.masonwang.pnp.entity.Team;
 import com.masonwang.pnp.entity.User;
 import com.masonwang.pnp.service.UserService;
 import lombok.AllArgsConstructor;
@@ -7,6 +9,9 @@ import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Set;
 
 @AllArgsConstructor
 @RestController
@@ -22,5 +27,31 @@ public class UserController {
     @PostMapping
     public ResponseEntity<User> saveUser(@RequestBody User user) {
         return new ResponseEntity<>(userService.saveUser(user), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User user) {
+        return new ResponseEntity<>(userService.updateUser(id, user), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<User>> getUsers() {
+        return new ResponseEntity<>(userService.getUsers(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}/teams")
+    public ResponseEntity<Set<Team>> getUserTeams(@PathVariable Long id) {
+        return new ResponseEntity<>(userService.getUserTeams(id), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}/proposals")
+    public ResponseEntity<Set<Proposal>> getUserProposals(@PathVariable Long id) {
+        return new ResponseEntity<>(userService.getUserProposals(id), HttpStatus.OK);
     }
 }
