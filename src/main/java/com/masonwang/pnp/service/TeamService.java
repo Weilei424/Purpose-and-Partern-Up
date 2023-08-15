67d3efc831b25f10793d78fef9fc7c0b9fbb308d
Zhenxu Wang <mason123wp@gmail.com>
2023-08-15 03:30:24 -0400

    implement methods

--- src/main/java/com/masonwang/pnp/service/TeamService.java
+++ src/main/java/com/masonwang/pnp/service/TeamService.java
@@ -1,7 +1,12 @@
 package com.masonwang.pnp.service;
 
+import com.masonwang.pnp.entity.Proposal;
 import com.masonwang.pnp.entity.Team;
+import com.masonwang.pnp.entity.User;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Set;
 
 public interface TeamService {
     Team getTeam(Long id);
@@ -9,5 +14,6 @@ public interface TeamService {
     void deleteTeam(Long id);
     Team updateTeam(Long id, Team team);
     Team addMember(Long teamId, Long userId);
-
+    List<Proposal> getTeamProposals();
+    Set<User> getTeamUsers();
 }
--- src/main/java/com/masonwang/pnp/service/TeamServiceImpl.java
+++ src/main/java/com/masonwang/pnp/service/TeamServiceImpl.java
@@ -1,10 +1,17 @@
 package com.masonwang.pnp.service;
 
+import com.masonwang.pnp.entity.Proposal;
 import com.masonwang.pnp.entity.Team;
+import com.masonwang.pnp.entity.User;
+import com.masonwang.pnp.exception.EntityNotFoundException;
 import com.masonwang.pnp.repository.TeamRepository;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -14,17 +21,17 @@ public class TeamServiceImpl implements TeamService {
 
     @Override
     public Team getTeam(Long id) {
-        return null;
+        return unwrapTeam(teamRepository.findById(id), id);
     }
 
     @Override
     public Team saveTeam(Team team) {
-        return null;
+        return teamRepository.save(team);
     }
 
     @Override
     public void deleteTeam(Long id) {
-
+        teamRepository.deleteById(id);
     }
 
     @Override
@@ -35,5 +42,20 @@ public class TeamServiceImpl implements TeamService {
     @Override
     public Team addMember(Long teamId, Long userId) {
         return null;
+    }
+
+    @Override
+    public List<Proposal> getTeamProposals() {
+        return null;
+    }
+
+    @Override
+    public Set<User> getTeamUsers() {
+        return null;
+    }
+
+    static Team unwrapTeam(Optional<Team> entity, Long id) {
+        if (entity.isPresent()) return entity.get();
+        else throw new EntityNotFoundException(id, Team.class);
     }
 }
