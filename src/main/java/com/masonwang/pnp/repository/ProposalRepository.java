127ad46a62bdd06272316b77e0576f4cd007a4f7
Zhenxu Wang <mason123wp@gmail.com>
2023-08-25 22:11:19 -0400

    add more crud methods

--- src/main/java/com/masonwang/pnp/repository/ProposalRepository.java
+++ src/main/java/com/masonwang/pnp/repository/ProposalRepository.java
@@ -1,9 +1,18 @@
 package com.masonwang.pnp.repository;
 
 import com.masonwang.pnp.entity.Proposal;
+import jakarta.transaction.Transactional;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface ProposalRepository extends CrudRepository<Proposal, Long> {
+    Optional<Proposal> findByUserIdAndTeamId(Long userId, Long teamId);
+    List<Proposal> findByUserId(Long userId);
+    List<Proposal> findByTeamId(Long teamId);
+    @Transactional
+    void deleteByUserIdAndTeamId(Long userId, Long teamId);
 }
