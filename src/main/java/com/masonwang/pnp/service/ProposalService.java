b1e2067a54ff7964831609aaba39531b91194f93
Zhenxu Wang <mason123wp@gmail.com>
2023-08-30 20:13:31 -0400

    add deleteUserProposal()

--- src/main/java/com/masonwang/pnp/service/ProposalService.java
+++ src/main/java/com/masonwang/pnp/service/ProposalService.java
@@ -9,6 +9,7 @@ public interface ProposalService {
     Proposal getProposal(Long id);
     Proposal saveProposal(Long userId, Long teamId, Proposal proposal);
     void deleteProposal(Long id);
+    void deleteUserProposal(Long proposalId, Long userId);
     Proposal updateProposal(Long id, Proposal proposal);
     Proposal updateProposalName(Long id, String name);
     Proposal updateProposalDescription(Long id, String description);
