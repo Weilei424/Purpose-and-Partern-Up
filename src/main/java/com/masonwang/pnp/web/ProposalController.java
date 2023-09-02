0c1921eed37f6a50dc732d693002c15c01ba1d10
Zhenxu Wang <mason123wp@gmail.com>
2023-09-02 14:53:57 -0400

    modify mapping annotations

--- src/main/java/com/masonwang/pnp/web/ProposalController.java
+++ src/main/java/com/masonwang/pnp/web/ProposalController.java
@@ -11,6 +11,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,57 +24,62 @@ import java.util.List;
 public class ProposalController {
     private ProposalService proposalService;
 
-    @ApiResponses(value ={
+    @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Successful retrieval of the proposal by id", content = @Content(array = @ArraySchema(schema = @Schema(implementation = Proposal.class)))),
             @ApiResponse(responseCode = "404", description = "Proposal does not exist", content = @Content(array = @ArraySchema(schema = @Schema(implementation = ErrorResponse.class)))),
     })
     @Operation(summary = "Get proposal by id", description = "Return a proposal based on an id")
-    @GetMapping("/{id}")
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Proposal> getProposal(@PathVariable Long id) {
         return new ResponseEntity<>(proposalService.getProposal(id), HttpStatus.OK);
     }
 
-    @PostMapping("/user/{userId}/team/{teamId}")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Successful save of the proposal"),
+            @ApiResponse(responseCode = "404", description = "Team or/and User id does not exist", content = @Content(array = @ArraySchema(schema = @Schema(implementation = ErrorResponse.class)))),
+    })
+    @Operation(summary = "Save a proposal", description = "Save proposal with required info")
+    @PostMapping(value = "/user/{userId}/team/{teamId}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Proposal> saveProposal(@PathVariable Long userId, @PathVariable Long teamId, @RequestBody Proposal proposal) {
         return new ResponseEntity<>(proposalService.saveProposal(userId, teamId, proposal), HttpStatus.CREATED);
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<HttpStatus> deleteProposal(@PathVariable Long id) {
         proposalService.deleteProposal(id);
 
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
-    @DeleteMapping("/{proposalId}/{userId}")
+    @DeleteMapping(value = "/{proposalId}/{userId}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<HttpStatus> deleteUserProposal(@PathVariable Long proposalId, @PathVariable Long userId) {
         proposalService.deleteUserProposal(proposalId, userId);
 
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
-    @PutMapping("/{id}")
+    @PutMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Proposal> updateProposal(@PathVariable Long id, @RequestBody Proposal proposal) {
         return new ResponseEntity<>(proposalService.updateProposal(id, proposal), HttpStatus.OK);
     }
 
-    @PutMapping("/{id}/name")
+    @PutMapping(value = "/{id}/name", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Proposal> updateProposalName(@PathVariable Long id, @RequestBody Proposal proposal) {
         return new ResponseEntity<>(proposalService.updateProposalName(id, proposal.getName()), HttpStatus.OK);
     }
 
-    @PutMapping("/{id}/description")
+    @PutMapping(value = "/{id}/description", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Proposal> updateProposalDescription(@PathVariable Long id, @RequestBody Proposal proposal) {
         return new ResponseEntity<>(proposalService.updateProposalDescription(id, proposal.getDescription()), HttpStatus.OK);
     }
 
-    @PutMapping("/{id}/team")
+    @PutMapping(value = "/{id}/team", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Proposal> updateProposalTeam(@PathVariable Long id, @RequestBody Proposal proposal) {
         //todo unfinished
         return new ResponseEntity<>(proposalService.updateProposalTeam(id, proposal.getTeam()), HttpStatus.OK);
     }
 
-    @GetMapping("/all")
+    @GetMapping(value = "/all", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<Proposal>> getProposals() {
         return new ResponseEntity<>(proposalService.getProposals(), HttpStatus.OK);
     }
