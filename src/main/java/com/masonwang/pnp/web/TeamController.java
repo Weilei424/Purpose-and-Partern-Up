5efbf5aa2291c16979fed539baeda416a278dd73
Zhenxu Wang <mason123wp@gmail.com>
2023-09-04 21:43:08 -0400

    implement openapi annotations for getTeam(), getTeamByName() and saveTeam()

--- src/main/java/com/masonwang/pnp/web/TeamController.java
+++ src/main/java/com/masonwang/pnp/web/TeamController.java
@@ -4,11 +4,19 @@ import com.masonwang.pnp.entity.Proposal;
 import com.masonwang.pnp.entity.Team;
 import com.masonwang.pnp.entity.User;
 import com.masonwang.pnp.service.TeamService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.*;
 
+import javax.print.attribute.standard.Media;
 import java.util.List;
 import java.util.Set;
 
@@ -18,19 +26,31 @@ import java.util.Set;
 public class TeamController {
     private TeamService teamService;
 
-    @GetMapping("/{id}")
+    @Operation(summary = "Get team by id", description = "Get team by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful retrieval of team by id", content = @Content(schema = @Schema(implementation = Team.class))),
+            @ApiResponse(responseCode = "404", description = "Team id does not exist", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+    })
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Team> getTeam(@PathVariable Long id) {
         return new ResponseEntity<>(teamService.getTeam(id), HttpStatus.OK);
     }
 
-    @GetMapping("/name")
-    public ResponseEntity<Team> getTeam(@RequestBody String name) {
+    @Operation(summary = "Get team by team name", description = "Get team by team name")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful retrieval of team by team name", content = @Content(schema = @Schema(implementation = Team.class))),
+            @ApiResponse(responseCode = "404", description = "Team id does not exist", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+    })
+    @GetMapping(value = "/name", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Team> getTeamByName(@RequestBody String name) {
         return new ResponseEntity<>(teamService.getTeamByName(name), HttpStatus.OK);
     }
 
-    @PostMapping("/addBy/{userId}")
-    public ResponseEntity<Team> saveTeam(@PathVariable Long userId, @RequestBody Team team) {
-        return new ResponseEntity<>(teamService.saveTeam(userId, team), HttpStatus.CREATED);
+    @Operation(summary = "Save a team", description = "Save user with valid info")
+    @ApiResponse(responseCode = "201", description = "Successful save of team", content = @Content(schema = @Schema(implementation = Team.class)))
+    @PostMapping("/addBy/{teamId}")
+    public ResponseEntity<Team> saveTeam(@PathVariable Long teamId, @RequestBody Team team) {
+        return new ResponseEntity<>(teamService.saveTeam(teamId, team), HttpStatus.CREATED);
     }
 
     @DeleteMapping("/{id}")
