a8955fcb7e95294b2ace6ce02f4ed92baf7bfa0a
Zhenxu Wang <mason123wp@gmail.com>
2023-09-08 18:23:06 -0400

    add unique constraint on team name

--- src/main/java/com/masonwang/pnp/entity/Team.java
+++ src/main/java/com/masonwang/pnp/entity/Team.java
@@ -22,7 +22,7 @@ public class Team {
 
     @NotBlank(message = "Team name cannot be blank!")
     @NonNull
-    @Column(name = "name", nullable = false)
+    @Column(name = "name", nullable = false, unique = true)
     private String name;
 
     @Column(name = "Contact")
