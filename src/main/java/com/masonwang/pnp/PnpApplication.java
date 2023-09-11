d2123f3ba252878dc3ce242051f384cc17dbc9f0
Zhenxu Wang <mason123wp@gmail.com>
2023-09-11 18:17:07 -0400

    add bean for pw encoder

--- src/main/java/com/masonwang/pnp/PnpApplication.java
+++ src/main/java/com/masonwang/pnp/PnpApplication.java
@@ -1,8 +1,12 @@
 package com.masonwang.pnp;
 
+import lombok.AllArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+@AllArgsConstructor
 @SpringBootApplication
 public class PnpApplication {
 
@@ -10,4 +14,8 @@ public class PnpApplication {
 		SpringApplication.run(PnpApplication.class, args);
 	}
 
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
