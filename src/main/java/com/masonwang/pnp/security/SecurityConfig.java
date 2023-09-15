5f5e62c88d8a6ce59eca32d910b9ae7a652cc709
Zhenxu Wang <mason123wp@gmail.com>
2023-09-14 21:14:17 -0400

    wire up CustomAuthenticationConverter

--- src/main/java/com/masonwang/pnp/security/SecurityConfig.java
+++ src/main/java/com/masonwang/pnp/security/SecurityConfig.java
@@ -1,4 +1,28 @@
 package com.masonwang.pnp.security;
 
+import com.masonwang.pnp.security.converter.CustomAuthenticationConverter;
+import com.masonwang.pnp.security.manager.CustomAuthenticationManager;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationConverter;
+import org.springframework.security.web.authentication.AuthenticationFilter;
+
+@AllArgsConstructor
+@Configuration
 public class SecurityConfig {
+
+    private final CustomAuthenticationManager customAuthenticationManager;
+    private final CustomAuthenticationConverter customAuthenticationConverter;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter(customAuthenticationManager, customAuthenticationConverter);
+
+        return null;
+    }
 }
